d4537c9b66cfff9d935eafc6d0fcb6c55fbd57cc
jmoore <josh@glencoesoftware.com>
2017-04-20 11:23:07 +0200

    Activate sanity checking

--- test.sh
+++ test.sh
@@ -1,1 +1,3 @@
 docker run -it --rm -v $PWD:/src jekyll/jekyll jekyll build -s /src
+docker run -it --rm -v $PWD:/src jekyll/jekyll bundle exec htmlproofer /src/_site
+docker run -it --rm -v $PWD:/src jekyll/jekyll check-links /src/_site
