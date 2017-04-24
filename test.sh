9e2f1b9ee60f2e73866a723e25227fb391c9514a
jmoore <josh@glencoesoftware.com>
2017-04-24 12:01:53 +0200

    Refactor test

--- test.sh
+++ test.sh
@@ -1,3 +1,1 @@
-docker run -it --rm -v $PWD:/src jekyll/jekyll jekyll build -s /src
-docker run -it --rm -v $PWD:/src jekyll/jekyll bundle exec htmlproofer /src/_site
-# docker run -it --rm -v $PWD:/src jekyll/jekyll check-links /src/_site
+docker run -it --rm -v $PWD:/src jekyll/jekyll bash /src/test.script
